6687313b9e37baac72a5bf64fe4d9bce22a294b1
jochoa-gaviria <jochoa.gaviria@hotmail.com>
2022-07-06 21:36:07 -0500

    First project example, first playground file

---
+++ FirstExample/FirstExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstExample
+//
+//  Created by Juan on 6/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

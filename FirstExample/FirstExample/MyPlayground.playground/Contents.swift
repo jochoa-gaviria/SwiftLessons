e1aa61ca6211d5f65929dcaa60101736bd19f55c
jochoa-gaviria <jochoa.gaviria@hotmail.com>
2022-07-09 13:03:34 -0500

    finish the third section, basics

--- FirstExample/FirstExample/MyPlayground.playground/Contents.swift
+++ FirstExample/FirstExample/MyPlayground.playground/Contents.swift
@@ -1,3 +1,35 @@
-import UIKit
+import UIKit //Library to create user interfaces
 
+//Begin Variables
 var greeting = "Hello, playground"
+greeting = "Hola, mundo!!"
+//End Variables
+
+
+//Begin Constants
+let myName = "Hello, world with const",  myBornDate = "02-08-1998",  myAge = 24 // Constants with multiple declarations
+// strConst = "Try to change a const" //We get an error.
+//End Constants
+
+//Begin datatypes
+//Integers
+var x = 18
+
+//Floats
+var y = 18.8
+
+//Arithmetic operators
+var sumInteger = x + 2
+var sumFLoats = y + 2.2
+var sumInt = Double(x) + y // We must to cast the type expected
+var sumDouble = x + Int(y) // We must to cast the type expected
+var division = Double(x) / 5 // We must to cast the type expected
+
+//Strings
+var myString = "String"
+//End datatypes
+
+
+//Explicit assignment
+var w:Int = 18
+var z:Double = 20.1

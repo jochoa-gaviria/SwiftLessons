806189ad4355bd341fac8cee0a9fdd989f9f2e76
jochoa-gaviria <jochoa.gaviria@hotmail.com>
2022-11-08 18:09:15 -0500

    Pickers and page controls

--- iOSProjectExample/iOSProjectExample/ViewController.swift
+++ iOSProjectExample/iOSProjectExample/ViewController.swift
@@ -11,7 +11,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var etiqueta: UILabel!
     @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var myPickerView: UIPickerView!
     
+    @IBOutlet weak var myPageControl: UIPageControl!
+    //Variables
+    
+    private let myPickerViewValues = ["Uno", "Dos", "Tres", "Cuatro", "Cinco"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +24,15 @@ class ViewController: UIViewController {
         myButton.setTitle("My Botón", for: .normal)
         myButton.backgroundColor = .blue
         myButton.setTitleColor(.white, for: .normal)
+        
+        myPickerView.dataSource = self
+        myPickerView.delegate = self
+        
+        myPageControl.numberOfPages = myPickerViewValues.count
+        
+        myPageControl.currentPageIndicatorTintColor = .blue
+        
+        myPageControl.pageIndicatorTintColor = .lightGray
     }
     
     @IBAction func myButtonAction(_ sender: Any) {
@@ -32,5 +46,35 @@ class ViewController: UIViewController {
     @IBAction func cambiarTexto(_ sender: Any) {
         etiqueta.text = "cambio"
     }
+    
+    
+    @IBAction func myPageControlAction(_ sender: Any) {
+        myPickerView.selectRow(myPageControl.currentPage, inComponent: 0, animated: true)
+    }
+    
+}
+
+
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate{
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        myPickerViewValues.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return myPickerViewValues[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let myString = myPickerViewValues[row]
+        myButton.setTitle(myString, for: .normal)
+        
+        myPageControl.currentPage = row
+    }
+    
 }
 

2bb3c780620cc784ea2753a5583cec9407cc4041
jochoa-gaviria <jochoa.gaviria@hotmail.com>
2022-11-22 20:34:30 -0500

    ScrollViews, WebViews and MapViews

--- ViewsNavigationControls/ViewsNavigationControls/ViewController.swift
+++ ViewsNavigationControls/ViewsNavigationControls/ViewController.swift
@@ -6,16 +6,19 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var myView: UIView!
     @IBOutlet weak var myImageView: UIImageView!
     @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var myWebView: WKWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        myWebView.load(URLRequest(url: URL(string: "https://www.google.com")!))
     }
 
 

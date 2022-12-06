ff411b05a437d5388ce457a7c89af4328e8bd7a2
jochoa-gaviria <jochoa.gaviria@hotmail.com>
2022-12-05 20:29:24 -0500

    Lineal navigation

---
+++ navigation/navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  navigation
+//
+//  Created by Juan on 5/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

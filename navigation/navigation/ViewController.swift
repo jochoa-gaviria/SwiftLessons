77765d6296d2e8c053a3aeb318d85ad5452de5cb
jochoa-gaviria <jochoa.gaviria@hotmail.com>
2022-12-06 20:39:18 -0500

    Data between screens

--- navigation/navigation/ViewController.swift
+++ navigation/navigation/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func greenButton(_ sender: Any) {
+        performSegue(withIdentifier: "greenScreen", sender: self)
+    }
+    @IBAction func blackButton(_ sender: Any) {
+        performSegue(withIdentifier: "blackScreen", sender: self)
+    }
+    @IBAction func redButton(_ sender: Any) {
+        performSegue(withIdentifier: "redScreen", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        
+        switch segue.identifier{
+        case "greenScreen":
+            if let destination = segue.destination as? ViewControllerGreenScreen{
+                destination.titulo = "Verde"
+            }
+        case "blackScreen":
+            if let destination = segue.destination as? ViewControllerBlackScreen{
+                destination.titulo = "Negro"
+            }
+        case "redScreen":
+            if let destination = segue.destination as? ViewControllerRedScreen{
+                destination.titulo = "Rojo"
+            }
+        case .none:
+            break
+        case .some(_):
+            break
+        }
+        
+    }
 }
 
---
+++ navigation/navigation/ViewControllerGreenScreen.swift
@@ -0,0 +1,33 @@
+//
+//  ViewControllerGreenScreen.swift
+//  navigation
+//
+//  Created by Juan on 6/12/22.
+//
+
+import UIKit
+
+class ViewControllerGreenScreen: UIViewController {
+
+    var titulo:String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let title = titulo{
+            self.title = title
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
